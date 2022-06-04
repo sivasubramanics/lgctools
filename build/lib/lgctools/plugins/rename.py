bd3e62bcb2d1c24372482e2f5d97122e4f6964ac
Sivasubramani S <siva.subramani975@gmail.com>
2022-06-04 15:42:44 +0200

    retrace big

--- build/lib/lgctools/plugins/rename.py
+++ build/lib/lgctools/plugins/rename.py
@@ -4,8 +4,8 @@ def rename_data(smdata, msdata, samplemap_dict):
     out_smdata = smdata.copy()
 
     for marker in msdata:
-        print(f"{sample}\t{marker}")
         for sample in msdata[marker].data:
+            print(f"{sample}\t{marker}")
             if sample in samplemap_dict:
                 out_msdata[marker].data[samplemap_dict[sample]
                                         ] = out_msdata[marker].data.pop(sample)
